dd1345c8476f9cda546e191201086bb11df3837f
Vikram Nadig <vikram.nadig@energyhelpline.com>
2018-06-13 17:22:09 +0100

    populate the post details on state updated with valid post

--- frontend/src/components/PostDetail.js
+++ frontend/src/components/PostDetail.js
@@ -16,7 +16,20 @@ class PostDetail extends Component {
     }
 
     render() {
-        return ( < h1 / > );
+        return (
+            <div>
+                {this.state.post &&
+                <div>
+                    <input className="post-title" value={this.state.post.title} onChange={()=>{}}/>
+                    <input className="post-body" value={this.state.post.body} onChange={()=>{}} />
+                    <input className="post-author" value={this.state.post.author} onChange={()=>{}} />
+                    <input className="post-commentCount" value={this.state.post.comments.length} onChange={()=>{}}/>
+                    <input className="post-currentScore" value={this.state.post.currentScore} onChange={()=>{}} />
+                    <input type="button" className="post-vote" />
+                    <input type="button" className="post-delete" />
+                </div>}
+            </div>
+        );
     };
 }
 PostDetail.propTypes = {
--- frontend/src/components/PostDetail.test.js
+++ frontend/src/components/PostDetail.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import PostDetail from './PostDetail';
 
-describe('given a category component', () => {
+const postDetail = {title:"postTitle",body:"post body goes here",author:"author-1",currentScore:2,comments:[]};
 
-    const postDetail = {title:"postTitle",body:"post body goes here",author:"author-1",currentScore:2};
+describe('given a <PostDetail> component is mounted', () => {
+
     const spy = jest.spyOn(PostDetail.prototype, 'componentDidMount');
     const onLoadMock = jest.fn()
         .mockImplementation(() => Promise.resolve({ data: postDetail }));
@@ -18,6 +19,25 @@ describe('given a category component', () => {
         expect(component.props().onLoad).toEqual(onLoadMock);
         expect(onLoadMock).toHaveBeenCalled();
         expect(PostDetail.prototype.componentDidMount).toHaveBeenCalled();
+    });
+
+    it('verifies the component state is populated', () => {
+        expect(component.state().post).toEqual(postDetail);
+    });
+});
+
+describe('<PostDetail> component on state update', () => {
+
+    const component = shallow(< PostDetail onLoad={() => Promise.resolve({})} />);
+    component.setState({ post: postDetail });
+
+    it('renders the post details', () => {
+        expect(component.find('.post-title').length).toEqual(1);
+        expect(component.find('.post-commentCount').length).toEqual(1);
+        expect(component.find('.post-author').length).toEqual(1);
+        expect(component.find('.post-currentScore').length).toEqual(1);
+        expect(component.find('.post-vote').length).toEqual(1);
+        expect(component.find('.post-delete').length).toEqual(1);
     });
 
 });
