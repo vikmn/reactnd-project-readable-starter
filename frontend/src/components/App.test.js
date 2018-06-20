381c5d27023b18c32f7073a1811aa6ea647259d5
Vikram Nadig <vikram.nadig@energyhelpline.com>
2018-06-20 12:46:01 +0100

    add default route to load up category page

--- frontend/src/components/App.test.js
+++ frontend/src/components/App.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
+import { MemoryRouter } from 'react-router';
 import App from './App';
+import Category from './Category';
+describe('default route for home page ', () => {
+  const component = mount(<MemoryRouter><App/></MemoryRouter>);
 
-it('renders without crashing', () => {
-  const component = shallow( < App / > );
-  expect(component.exists()).toEqual(true);
+  it('render the category page', () => {
+    expect(component.exists()).toEqual(true);
+    expect(component.find(Category).length).toEqual(1);
+  });
+
 });
