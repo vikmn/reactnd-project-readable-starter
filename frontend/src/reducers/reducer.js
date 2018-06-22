0d31882c3b01891f2e9485686e852fe32044abfb
Vikram Nadig <vikram.nadig@energyhelpline.com>
2018-06-22 17:57:08 +0100

    WIP create reducers with tests

--- frontend/src/reducers/reducer.js
+++ frontend/src/reducers/reducer.js
@@ -1,5 +1,37 @@
-export const initialState = {};
+import { POST } from "../actions/types";
 
-export const reducer = (state = initialState, action) => state;
+export const initialState = {
+    posts: {
+        "2": {
+            id: 2,
+            votes: 3
+        }
+    }
+};
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case POST.CREATE:
+            return {
+                ...state,
+                posts: {
+                    ...state.posts,
+                    [action.id]: action.post
+                }
+        }; 
+        case POST.UPVOTE:
+            return {
+                ...state,
+                posts: {
+                    ...state.posts,
+                    [action.id]:{
+                        id: action.id,
+                        votes: state.posts[action.id].votes +1,
+                    }
+                }
+        }; 
+        default: return state;
+    }
+};
 
 export default reducer;
--- frontend/src/reducers/reducers.test.js
+++ frontend/src/reducers/reducers.test.js
@@ -1,8 +1,41 @@
-import { } from '../actions/index'
-import { } from '../actions/types'
+import { POST } from '../actions/types'
 import { reducer, initialState } from './reducer';
+
 describe('Reducer', () => {
     it('Should return the initial state when no action passed', () => {
-      expect(reducer(undefined, {})).toEqual(initialState);
+        expect(reducer(undefined, {})).toEqual(initialState);
     });
-  });
+});
+
+describe('Create post', () => {
+    it('should return correct state', () => {
+        const action = {
+            type: POST.CREATE,
+            id: 1,
+            post: {
+                id: 1,
+                votes: 1,
+            }
+        };
+        const expectedState = {
+            ...action.post
+        };
+        expect(reducer(undefined, action).posts[action.id]).toEqual(expectedState);
+    });
+
+    it('upvoting a post should return updated post count', () => {
+        const action = {
+            type: POST.UPVOTE,
+            id: 2,
+            post: {
+                votes: 1
+            }
+        };
+
+        const expectedState = {
+                id: action.id,
+                votes: 4
+        };
+        expect(reducer(undefined, action).posts[action.id]).toEqual(expectedState);
+    });
+});
