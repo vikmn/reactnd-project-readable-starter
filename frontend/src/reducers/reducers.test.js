5bdaa20e2016fecb28f2e8aab71ae4e273e379cf
Vikram Nadig <vikram.nadig@energyhelpline.com>
2018-06-27 10:46:13 +0100

    update post reducer tests to use the real actions

--- frontend/src/reducers/reducers.test.js
+++ frontend/src/reducers/reducers.test.js
@@ -1,5 +1,5 @@
-import { POST } from '../actions/types'
 import { reducer } from './reducer';
+import { postActions } from '../actions/.';
 
 describe('Reducer', () => {
     it('Should return the initial state when no action passed', () => {
@@ -11,82 +11,78 @@ describe('Create post when post does not exist', () => {
     it('should create a post', () => {
 
         const initialState = { posts: {} }; 
-        const action = {
-            type: POST.CREATE,
-            id: 1,
-            post: {
+        const action = postActions.createPost({
                 id: 1,
                 votes: 1,
-            }
-        };
+        });
+        
         const expectedState = {
             ...action.post
         };
+
         expect(reducer(initialState, action).posts[action.id]).toEqual(expectedState);
     });
 
     it('upvoting an existing post should return updated post count', () => {
+
+        const postId = 2;
         const initialState = {
             posts: {
                 "2": {
-                    id: 2,
+                    id: postId,
                     votes: 3
                 }
             }
         };
 
-        const action = {
-            type: POST.UPVOTE,
-            id: 2,
-        };
+        const action = postActions.upvotePost(postId);
 
         const expectedState = {
-                id: action.id,
+                id: postId,
                 votes: 4
         };
-        expect(reducer(initialState, action).posts[action.id]).toEqual(expectedState);
+
+        expect(reducer(initialState, action).posts[postId]).toEqual(expectedState);
     });
     
     it('downvoting a post should return decremented vote count', () => {
+
+        const postId = 2;
         const initialState = {
             posts: {
                 "2": {
-                    id: 2,
+                    id: postId,
                     votes: 3
                 }
             }
         };
-
-        const action = {
-            type: POST.DOWNVOTE,
-            id: 2
-        };
+        const action = postActions.downvotePost(postId);
 
         const expectedState = {
-                id: action.id,
+                id: postId,
                 votes: 2
         };
-        expect(reducer(initialState, action).posts[action.id]).toEqual(expectedState);
+
+        expect(reducer(initialState, action).posts[postId]).toEqual(expectedState);
     });
     
     it('downvoting a post should not decrement vote count when 0', () => {
+        const postId = 2;
         const initialState = {
             posts: {
                 "2": {
-                    id: 2,
+                    id: postId,
                     votes: 0
                 }
             }
         };
-        const action = {
-            type: POST.DOWNVOTE,
-            id: 2
-        };
+        const action = postActions.downvotePost(postId);
 
         const expectedState = {
-                id: action.id,
+                id: postId,
                 votes: 0
         };
-        expect(reducer(initialState, action).posts[action.id]).toEqual(expectedState);
+
+        expect(reducer(initialState, action).posts[postId]).toEqual(expectedState);
     });
 });
