921d47a8c9b6bbbaa3d5955ce3d142e3580acef3
Vikram Nadig <vikram.nadig@energyhelpline.com>
2018-06-27 17:13:25 +0100

    install redux and use react-reduce for provisoning of state

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,17 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import reducer from './reducers/reducer';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
+    registerServiceWorker();
