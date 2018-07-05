2777a5d248dcb5ccca087c36c42094451b534e6c
Vikram Nadig <vikram.nadig@energyhelpline.com>
2018-07-05 18:36:01 +0100

    connect up the PostDetails component to redux

--- frontend/src/components/PostDetail.js
+++ frontend/src/components/PostDetail.js
@@ -1,18 +1,10 @@
 import React, { Component } from 'react';
-import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
 
 class PostDetail extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { post: null };
-    }
-
-    componentDidMount() {
-        this.props.onLoad()
-            .then(result => {
-                this.setState({ post: result.data });
-            })
     }
 
     updateVote() { }
@@ -20,15 +12,16 @@ class PostDetail extends Component {
     deletePost() { }
 
     render() {
+        const postDetails = this.props.post;
         return (
             <div>
-                {this.state.post &&
+                {postDetails &&
                 <div>
-                    <input className="post-title" value={this.state.post.title} onChange={()=>{}}/>
-                    <input className="post-body" value={this.state.post.body} onChange={()=>{}} />
-                    <input className="post-author" value={this.state.post.author} onChange={()=>{}} />
-                    <input className="post-commentCount" value={this.state.post.comments.length} onChange={()=>{}}/>
-                    <input className="post-currentScore" value={this.state.post.currentScore} onChange={()=>{}} />
+                    <input className="post-title" value={postDetails.title} onChange={()=>{}}/>
+                    <input className="post-body" value={postDetails.body} onChange={()=>{}} />
+                    <input className="post-author" value={postDetails.author} onChange={()=>{}} />
+                    <input className="post-commentCount" value={postDetails.comments.length} onChange={()=>{}}/>
+                    <input className="post-currentScore" value={postDetails.currentScore} onChange={()=>{}} />
                     <input type="button" className="post-vote" onClick={ this.updateVote } />
                     <input type="button" className="post-delete" onClick={ this.deletePost } />
                 </div>}
@@ -36,8 +29,16 @@ class PostDetail extends Component {
         );
     };
 }
-PostDetail.propTypes = {
-    onLoad : PropTypes.func
-}
 
-export default PostDetail;
+const mapStateToProps = (state) =>
+    ({
+        post: {
+            title: "postTitle",
+            body: "post body goes here",
+            author: "author-1",
+            currentScore: 2,
+            comments: []
+        }
+    });
+
+export default connect(mapStateToProps)(PostDetail);
--- frontend/src/components/PostDetail.test.js
+++ frontend/src/components/PostDetail.test.js
@@ -1,64 +1,56 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import PostDetail from './PostDetail';
+import { createStore } from 'redux';
+import reducer from '../reducers/reducer';
 
 const postDetail = {title:"postTitle",body:"post body goes here",author:"author-1",currentScore:2,comments:[]};
 
 describe('given a <PostDetail> component is mounted', () => {
 
-    const componentDidMountSpy = jest.spyOn(PostDetail.prototype, 'componentDidMount');
-    const onLoadMock = jest.fn()
-        .mockImplementation(() => Promise.resolve({ data: postDetail }));
-    const component = mount(< PostDetail onLoad={ onLoadMock } />);
+    const store = createStore(reducer);
+
+    const component = shallow(< PostDetail store={store}/>);
 
     it('renders without crashing', () => {
         expect(component.exists()).toEqual(true);
     });
 
-    it('verifies the component props are set up', () => {
-        expect(component.props().onLoad).toEqual(onLoadMock);
-        expect(onLoadMock).toHaveBeenCalled();
-        expect(componentDidMountSpy).toHaveBeenCalled();
-    });
-
     it('verifies the component populates the post details', () => {
-        expect(component.state().post).toEqual(postDetail);
+        expect(component.props().post).toEqual(postDetail);
     });
 });
 
 describe('<PostDetail> component renders', () => {
 
-    const component = shallow(< PostDetail onLoad={() => Promise.resolve({})} />);
-    component.setState({ post: postDetail });
+    const store = createStore(reducer);
+
+    const component = shallow(< PostDetail store={store} />);
 
     it('renders the post details', () => {
-        expect(component.find('.post-title').length).toEqual(1);
-        expect(component.find('.post-commentCount').length).toEqual(1);
-        expect(component.find('.post-author').length).toEqual(1);
-        expect(component.find('.post-currentScore').length).toEqual(1);
+        expect(component.dive().find('.post-title').length).toEqual(1);
+        expect(component.dive().find('.post-author').length).toEqual(1);
+        expect(component.dive().find('.post-currentScore').length).toEqual(1);
+        expect(component.dive().find('.post-commentCount').length).toEqual(1);
     });
 });
 
 describe('<PostDetail> component on vote click', () => {
-    const voteSpy = jest.spyOn(PostDetail.prototype, 'updateVote');
 
-    const component = shallow(< PostDetail onLoad={() => Promise.resolve({})} />);
-    component.setState({ post: postDetail });
+    const store = createStore(reducer);
+    const component = shallow(< PostDetail store={store} />);
 
     it('handles voting on vote click', () => {
-        component.find('.post-vote').simulate('click');
-        expect(voteSpy).toHaveBeenCalled();
+        component.dive().find('.post-vote').simulate('click');
     });
 });
 
 describe('<PostDetail> component on delete post', () => {
-    const deleteSpy = jest.spyOn(PostDetail.prototype, 'deletePost');
 
-    const component = shallow(< PostDetail onLoad={() => Promise.resolve({})} />);
-    component.setState({ post: postDetail });
+    const store = createStore(reducer);
+    const component = shallow(< PostDetail store={store} />);
 
     it('handles delete on post', () => {
-        component.find('.post-delete').simulate('click');
-        expect(deleteSpy).toHaveBeenCalled();
+        component.dive().find('.post-delete').simulate('click');
      });
 });
